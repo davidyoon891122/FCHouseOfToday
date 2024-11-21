6456d071184338b677ecfbf4898dd0a1e22c2ea3
David Yoon <qqpo12@naver.com>
2024-11-21 21:23:32 +0900

    Get data from the firestore
    * Get data from the firestore and convert to ArticleModel

--- app/src/main/java/com/example/fchouseoftoday/HomeFragment.kt
+++ app/src/main/java/com/example/fchouseoftoday/HomeFragment.kt
@@ -1,15 +1,31 @@
 package com.example.fchouseoftoday
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.fchouseoftoday.data.ArticleModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.toObject
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val db = Firebase.firestore
 
+        db.collection("articles").document("2xZK5PgFycebyjvusO9E")
+            .get()
+            .addOnSuccessListener { result ->
+                val article = result.toObject<ArticleModel>()
+
+                Log.e("HomeFragment", article.toString())
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+            }
     }
 
 }
---
+++ app/src/main/java/com/example/fchouseoftoday/data/ArticleModel.kt
@@ -0,0 +1,9 @@
+package com.example.fchouseoftoday.data
+
+data class ArticleModel(
+    val articleId: String? = null,
+    val createdAt: Long? = null,
+    val description: String? = null,
+    val imageUrl: String? = null,
+) {
+}
